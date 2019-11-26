87875c0ddab4a0c35a16f373ecd203e0b7d8d582
Naomi Airam Rangel Lizarraga <al17517@cbtis051.edu.mx>
2019-11-26 14:12:50 -0700

    Ajustes en la respuesta a formato JSON

--- app_al17517.js
+++ app_al17517.js
@@ -2,6 +2,14 @@ var http = require('http');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
 
-    res.end('Hola mundo Node JS Repo git - Rangel Lizarraga Naomi Airam\n');
+    var objeto = 
+    {
+        mensaje : 'Hola mundo Node JS Repo git - Rangel Lizarraga Naomi Airam\n'
+    }
+
+    var json = JSON.stringify(objeto);
+
+    res. end(json);
+    
 }).listen(8081);
 console.log('Server running on port 8081');
