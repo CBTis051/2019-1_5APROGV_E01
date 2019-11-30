5aacfceb7c672bdac306aa4b0c6a8e2d58299f2e
Marcos Miguel Luna Hernandez <al17505@cbtis051.edu.mx>
2019-11-29 18:55:07 -0700

    Ajustes en la respuesta a formato JSON

--- app_al17505.js
+++ app_al17505.js
@@ -2,6 +2,13 @@ var http = require('http');
 http.createServer(function (req, res) {
 res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
 
-res.end('Hola mundo Node JS Repo GIT - Luna Hernandez Marcos Miguel\n');
+var objeto = {
+    mensaje : 'Hola mundo Node JS Repo GIT - Luna Hernandez Marcos Miguel \n'
+};
+
+var json = JSON.stringify(objeto);
+
+res.end(json); 
+
 }).listen(8081);
 console.log('Server running on port 8081');
