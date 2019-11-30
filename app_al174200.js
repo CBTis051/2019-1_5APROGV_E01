92094b7bdecaa22a5f9f33687ec34ef2cf2836af
Ailyn Mikol Sarmiento Arias <al17420@cbtis051.edu.mx>
2019-11-30 00:28:03 -0700

    Commit inicial del server

--- app_al174200.js
+++ app_al174200.js
@@ -3,5 +3,5 @@ http.createServer(function (req, res) {
 res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
 
 res.end('Hola mundo Node JS - Sarmiento Arias Ailyn Mikol\n');
-}).listen(8080);
-console.log('Server running on port 8080');
+}).listen(8081);
+console.log('Server running on port 8081');
