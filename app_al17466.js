c392c5a11da3cb5429f3587b78022de395c58dd3
Mara Ivain Ramos Garcia <al17466@cbtis051.edu.mx>
2019-11-30 00:04:11 -0700

    Ajustes en la respuesta a formato JSON

--- app_al17466.js
+++ app_al17466.js
@@ -2,6 +2,13 @@ var http = require('http');
 http.createServer(function (req, res) {
 res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
 
-res.end('Hola mundo Node JS - Ramos Garcia Mara Ivain\n');
+var objeto = {
+    mensaje : 'Hola mundo Node JS Repo GIT - Ramos Garcia Mara Ivain \n'
+};
+
+var json = JSON.stringify(objeto);
+
+res.end(json); 
+
 }).listen(8081);
 console.log('Server running on port 8081');
