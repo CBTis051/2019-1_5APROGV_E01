4fdd2e0ca8611d2f80f5ef1debe79ae29385c761
Ailyn Mikol Sarmiento Arias <al17420@cbtis051.edu.mx>
2019-11-30 00:42:54 -0700

    Ajustes en la respuesta a formato JSON

--- app_al174200.js
+++ app_al174200.js
@@ -1,7 +1,15 @@
- var http = require('http');
+var http = require('http');
 http.createServer(function (req, res) {
-res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
+    res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
 
-res.end('Hola mundo Node JS - Sarmiento Arias Ailyn Mikol\n');
+    var objeto = 
+    {
+        mensaje : 'Hola mundo Node JS Repo git - Sarmiento Arias Ailyn Mikol\n'
+    }
+
+    var json = JSON.stringify(objeto);
+
+    res. end(json);
+    
 }).listen(8081);
 console.log('Server running on port 8081');
